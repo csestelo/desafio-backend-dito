2ad9ed5944246ef2e8c8818664f5da2e45f1d257
cintia_sestelo <cintia.sestelo@sieve.com.br>
2018-04-18 19:58:30 -0300

    adiciona inits e testes post

---
+++ tests/services/test_post.py
@@ -0,0 +1,23 @@
+from asynctest import TestCase
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from api import services
+from api.config import MONGO_URI, EVENTS_COLLECTION
+
+
+class ServicesTest(TestCase):
+    async def setUp(self):
+        client = AsyncIOMotorClient(MONGO_URI)
+        db = client['test']
+        self.collection = db[EVENTS_COLLECTION]
+
+    async def tearDown(self):
+        await self.collection.drop()
+
+    async def test_insert_event(self):
+        event_args = {'event': 'buy', 'timestamp': '5678'}
+        await services.insert_event(self.collection, event_args)
+
+        inserted_doc = await self.collection.find({}, {"_id": 0}).to_list(None)
+
+        assert inserted_doc == [event_args]
