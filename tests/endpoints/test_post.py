7833c2dfbef7c98a83b9a2239adf8d99a5dfcd14
cintia_sestelo <cintia.sestelo@sieve.com.br>
2018-04-18 19:55:29 -0300

    move arquivos e implementa testes de post

--- tests/endpoints/test_post.py
+++ tests/endpoints/test_post.py
@@ -1,5 +1,9 @@
+from unittest.mock import patch
+
 from aiohttp.test_utils import unittest_run_loop
 from http import HTTPStatus
+
+from asynctest import CoroutineMock
 
 from tests.base import BaseTests
 
@@ -7,11 +11,12 @@ from tests.base import BaseTests
 class PostTests(BaseTests):
     @unittest_run_loop
     async def test_post_with_correct_arguments_returns_200(self):
-        resp = await self.client.request('POST', '/events',
-                                         json={'event': 'buy', 'timestamp': ''})
-        assert resp.status == HTTPStatus.OK
-        text = await resp.json()
-        assert {"haha": "tchau"} == text
+        with patch('api.endpoints.insert_event', CoroutineMock()):
+            resp = await self.client.request('POST', '/events',
+                                             json={'event': 'buy',
+                                                   'timestamp': ''})
+
+        assert resp.status == HTTPStatus.CREATED
 
     @unittest_run_loop
     async def test_post_without_required_argument_returns_422(self):
