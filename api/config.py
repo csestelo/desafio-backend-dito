9c41c551e24285a5f7ad56b631c4b57280b61efe
csestelo <sestelos@gmail.com>
2018-04-17 23:50:51 -0300

    adiciona envs

--- api/config.py
+++ api/config.py
@@ -1,4 +1,8 @@
 import os
 
+APP_PORT = int(os.getenv('APP_PORT', 5000))
+
 MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
 EVENTS_COLLECTION = os.getenv('EVENTS_COLLECTION', 'events')
+MONGO_DB_NAME = os.getenv('MONGO_DB_NAME', 'backend_dito')
+
