7ff9377a159cae7dc16d276444df725dabb7ae93
csestelo <sestelos@gmail.com>
2018-04-18 23:16:18 -0300

    adiciona teste para formato de data

--- tests/endpoints/test_post.py
+++ tests/endpoints/test_post.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest.mock import patch
 
 from aiohttp.test_utils import unittest_run_loop
@@ -5,21 +6,33 @@ from http import HTTPStatus
 
 from asynctest import CoroutineMock
 
+from api.config import DATETIME_FORMAT
 from tests.base import BaseTests
 
 
 class PostTests(BaseTests):
     @unittest_run_loop
     async def test_post_with_correct_arguments_returns_200(self):
+        params = {'event': 'buy',
+                  'timestamp': datetime.now().strftime(DATETIME_FORMAT)}
+
         with patch('api.endpoints.insert_event', CoroutineMock()):
             resp = await self.client.request('POST', '/events',
-                                             json={'event': 'buy',
-                                                   'timestamp': ''})
+                                             json=params)
 
-        assert resp.status == HTTPStatus.CREATED
+        assert HTTPStatus.CREATED == resp.status
 
     @unittest_run_loop
     async def test_post_without_required_argument_returns_422(self):
         resp = await self.client.request('POST', '/events',
                                          json={'event': 'buy'})
+
+        assert resp.status == HTTPStatus.UNPROCESSABLE_ENTITY
+
+    @unittest_run_loop
+    async def test_post_with_invalid_argument_returns_422(self):
+        resp = await self.client.request('POST', '/events',
+                                         json={'event': 'buy',
+                                               'timestamp': 'invalid datetime'})
+
         assert resp.status == HTTPStatus.UNPROCESSABLE_ENTITY
