f8b5b5c67fdec296abae1f16e79e2144268e32f4
csestelo <sestelos@gmail.com>
2018-04-29 13:24:15 -0300

    modifica script para adicionar docs diretamente no banco

--- script/populate_db.py
+++ script/populate_db.py
@@ -2,43 +2,53 @@ import asyncio
 import random
 import time
 from datetime import datetime
+from itertools import repeat
 
-from aiohttp import ClientSession
-from http import HTTPStatus
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import List, Dict
 
-from api.config import INSERT_DOCS_QTY, DATETIME_FORMAT, POST_URL
+from api.config import INSERT_DOCS_QTY, DATETIME_FORMAT, MONGO_URI, \
+    EVENTS_COLLECTION, MONGO_DB_NAME, BULK_INSERTION_QTY
 
 POSSIBLE_EVENTS = ['buy', 'sell', 'watch', 'exchange', 'steal', 'dance',
                    'got rich', 'be jealous', 'swim', 'got naked']
 
+conn = AsyncIOMotorClient(MONGO_URI)
+collection = conn[MONGO_DB_NAME][EVENTS_COLLECTION]
 
-def create_random_datetime():
+
+def create_random_datetime_string() -> str:
     random_timestamp = round(random.uniform(0, time.time()), 2)
     datetime_string = datetime.fromtimestamp(random_timestamp)
     return datetime.strftime(datetime_string, DATETIME_FORMAT)
 
 
-def create_messages():
+def create_messages(qty: int) -> List[Dict]:
     return [{"event": random.choice(POSSIBLE_EVENTS),
-             "timestamp": create_random_datetime()}
-            for i in range(INSERT_DOCS_QTY)]
+             "timestamp": create_random_datetime_string()}
+            for i in range(qty)]
 
 
-async def insert_doc(message):
-    with ClientSession() as session:
-        resp = await session.post(POST_URL, json=message)
+async def insert_docs(messages: List[Dict]):
+    inserted = await collection.insert_many(messages)
+    print({"info": f'Inserted {len(inserted.inserted_ids)} docs.'})
 
-        if resp.status == HTTPStatus.CREATED:
-            print({"info": f'Inserted doc: {message}'})
-            return
 
-        print({"info": f'Was not possible insert doc: {message}',
-               "status_code": resp.status})
-        return
+def msgs_per_insertion(total: int = INSERT_DOCS_QTY,
+                       per_insertion: int = BULK_INSERTION_QTY) -> List[int]:
+    n_insertions, rest = divmod(total, per_insertion)
+    qtd = list(repeat(per_insertion, n_insertions))
+    if rest:
+        qtd.append(rest)
+    return qtd
 
+
+msgs_qty = msgs_per_insertion()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(asyncio.gather(
-    *(insert_doc(msg) for msg in create_messages())
-))
+    *(insert_docs(create_messages(qty)) for qty in msgs_qty))
+)
+
+conn.close()
 loop.close()
