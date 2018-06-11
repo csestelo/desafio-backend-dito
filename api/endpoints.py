6aa5b9835b5966d27eaaa447ce13b46979f26cca
cintia_sestelo <cintia.sestelo@sieve.com.br>
2018-06-11 20:14:20 -0300

    WIP - adicionar elementos HTML para autocomplete

--- api/endpoints.py
+++ api/endpoints.py
@@ -7,6 +7,13 @@ from api.config import EVENTS_COLLECTION, MONGO_DB_NAME, REDIS_TTL
 from api.schemas import PostSchema, GetSchema
 from api.services import insert_event, get_distinct_events
 
+CORS_HEADERS = {
+    "Access-Control-Allow-Origin": "*",
+    # "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "GET",
+    "Access-Control-Allow-Headers": "Content-Type"
+}
+
 
 @use_args(GetSchema())
 async def get(request, args):
@@ -17,17 +24,20 @@ async def get(request, args):
 
     if cached_result:
         return web.json_response(status=HTTPStatus.OK,
-                                 data={"events": json.loads(cached_result)})
+                                 data={"events": json.loads(cached_result)},
+                                 headers=CORS_HEADERS)
 
     collection = request.app['mongodb'][MONGO_DB_NAME][EVENTS_COLLECTION]
     events = await get_distinct_events(collection=collection,
                                        startswith=args['event_startswith'])
 
     if not events:
-        return web.json_response(status=HTTPStatus.NOT_FOUND)
+        return web.json_response(status=HTTPStatus.NOT_FOUND,
+                                 headers=CORS_HEADERS)
 
     await cache.set(key, json.dumps(events), expire=REDIS_TTL)
-    return web.json_response(status=HTTPStatus.OK, data={"events": events})
+    return web.json_response(status=HTTPStatus.OK, data={"events": events},
+                             headers=CORS_HEADERS)
 
 
 @use_args(PostSchema())
--- front/index.js
+++ front/index.js
@@ -4,17 +4,16 @@ document.onreadystatechange = function () {
         inputElement.oninput = function () {
             if (inputElement.value.length > 1) {
                 let inputVal = inputElement.value;
-                let myHeaders = new Headers({'Content-Type': 'application/json'});
-                // gambi, modificar após implementar CORS
-                let myInit = {method: 'GET', headers: myHeaders, mode: 'no-cors'};
                 let url = 'http://localhost:5000/events?event_startswith=' + inputVal;
-                let myRequest = new Request(url, myInit);
+                let myRequest = new Request(url);
 
-                fetch(myRequest).then(function (response) {
-                    response.json().then(function (json) {
-                            console.log(json)
+                fetch(myRequest)
+                    .then(function (response) {
+                        return response.json();
                     })
-                })
+                    .then(function (content) {
+                        console.log(content);
+                    })
             }
         }
     }
